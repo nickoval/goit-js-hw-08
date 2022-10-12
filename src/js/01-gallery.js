742c0e3cf5811f181dd87dc540799d0059c10ed8
nickoval <mykovalmail@gmail.com>
2022-10-12 20:59:08 +0300

    add simpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+console.log('SimpleLightbox', SimpleLightbox);
 console.log(galleryItems);
